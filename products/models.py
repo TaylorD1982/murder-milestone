496a35dd986638cef5c24b1a5b1dbc2e167d1356
Ubuntu <ubuntu@ip-172-31-84-5.ec2.internal>
2020-04-16 09:15:30 +0000

    Products page reformatted and additional fields added to model

--- products/models.py
+++ products/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
+    description2 = models.TextField(default='')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
-
+    
     def __str__(self):
         return self.name
