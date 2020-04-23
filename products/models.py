c63fe918a2003cee59adc555c3ed273e3049daf7
Ubuntu <ubuntu@ip-172-31-84-5.ec2.internal>
2020-04-23 13:23:30 +0000

    products model updated with no fields and modal styling completed

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     description2 = models.TextField(default='')
+    address = models.TextField(default='')
+    date = models.DateField(blank=True)
+    time = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
     
--- reviews/models.py
+++ reviews/models.py
@@ -6,10 +6,28 @@ class Review(models.Model):
     """
     A single Review
     """
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+    
+    EVENT_CHOICES = (
+        (1, 'Spectre in the Ballroom'),
+        (2, 'The Hunting Party'),
+        (3, 'The Corpse in the Drawing Room'),
+        (4, 'A Time to Kill'),
+        (5, 'The Casino Killing'),
+    )
+    
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
-    rating = models.SmallIntegerField()
+    rating = models.SmallIntegerField(choices=RATING_CHOICES)
+    event = models.TextField(default='', choices=EVENT_CHOICES)
+    
 
     def __unicode__(self):
         return self.title
